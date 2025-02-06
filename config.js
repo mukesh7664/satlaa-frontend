c1a34515f0aedc9203c314271407fd4cd81d8fd7
Mukesh Soni <mukeshsoni73362@gmail.com>
2025-02-06 16:36:07 +0530

    config

--- config.js
+++ config.js
@@ -30,11 +30,16 @@ const jsonConfig = {
 };
 
 if (process.env.NODE_ENV === "development") {
- 
+  jsonConfig.API_URL = "https://uat.satlaa.com";
+  jsonConfig.WEBSITE_URL = "http://localhost:3000";
+  jsonConfig.IMG_URL = "https://uat.satlaa.com";
+} else if (process.env.VERCEL_ENV === "fullstack"){
+  
   jsonConfig.API_URL = "http://localhost:5002";
   jsonConfig.WEBSITE_URL = "http://localhost:3000";
   jsonConfig.IMG_URL = "http://localhost:5002";
-} else if (process.env.VERCEL_ENV === "production") {
+}
+else if (process.env.VERCEL_ENV === "production") {
   jsonConfig.API_URL = "https://uat.satlaa.com";
   jsonConfig.WEBSITE_URL = "https://satlaa.vercel.app";
   jsonConfig.IMG_URL = "https://uat.satlaa.com";
