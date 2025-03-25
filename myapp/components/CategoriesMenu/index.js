1ce7b744b3d43b8aec10f7caed0a9ab4156a62d3
retr0717 <alwinaji717@gmail.com>
2025-03-25 06:48:33 +0530

    categories menu component to shadcn

--- myapp/components/CategoriesMenu/index.js
+++ myapp/components/CategoriesMenu/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Menu } from "@mui/material";
+import { Button } from "@/components/ui/button";
 import { useSelector } from "react-redux";
 import router from "next/router";
 
@@ -16,16 +16,18 @@ const Page = () => {
 
   return (
     <>
-      {categoriesData?.length > 0 ? (
-        <Menu mode="horizontal" className=" block" onClick={onClickMenu}>
-          {categoriesData?.map((val) => (
-            <Menu.Item key={val.seo}>
-              <a className="uppercase mr-2">{val.title}</a>
-            </Menu.Item>
+      {categoriesData?.length > 0 && (
+        <div className="flex flex-wrap gap-2" onClick={onClickMenu}>
+          {categoriesData.map((val) => (
+            <Button
+              key={val.seo}
+              variant="ghost"
+              className="uppercase mr-2"
+            >
+              {val.title}
+            </Button>
           ))}
-        </Menu>
-      ) : (
-        ""
+        </div>
       )}
     </>
   );
