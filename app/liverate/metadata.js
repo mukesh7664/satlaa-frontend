242f0200d221199933317b41a5d60b0bae89d5d2
Mukesh Soni <mukeshsoni73362@gmail.com>
2025-04-08 11:42:58 +0530

    64e6

--- app/liverate/metadata.js
+++ app/liverate/metadata.js
@@ -3,5 +3,5 @@ export const metadata = {
   keywords:
     "Gold rate today, Today’s gold rate, Gold price today, 1 gram gold rate today, GOLD rates/price Live, GOLD rate/price in India Live, GOLD price, GOLD rate today Live, gold price today, GOLD rates India, gold price per gram, GOLD chart, GOLD price per gram, GOLD funds, bullion stocks, GOLD Price India, GOLD Price Forecast",
   description:
-    "Get the latest gold rate today in India. Know today’s gold rate in Mumbai, Delhi, Chennai, Kerala and Bengaluru. Live gold price today in India and 1 gram gold rate today",
+    "Get the latest gold rate today in India. Know today’s gold rate in Mumbai, Delhi, Chennai, Kerala and Bengaluru. Live gold price today in India and 1 gram gold rate today ",
 };
